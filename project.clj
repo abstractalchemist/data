aeed8b797d7ecf84295641e37202e3b765226d4a
Jason Hirata <jhirata@integrity-apps.com>
2016-03-05 21:35:53 -1000

    modifying handler to check for JWT google token and basic data

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.4.0"]
+                 [ring-cors "0.1.7"]
                  [ring/ring-defaults "0.1.5"]
                  [clj-json "0.5.3"]
                  [com.google.api-client/google-api-client "1.19.1"]]
--- src/data/handler.clj
+++ src/data/handler.clj
@@ -4,6 +4,8 @@
            [com.google.api.client.json.jackson2 JacksonFactory])
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.middleware [cors :refer [wrap-cors]]]
+            [clojure.pprint]
             [clj-json [core :as json]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
@@ -26,22 +28,79 @@
     (when-let [token (. verifier verify idtoken)]
       (let [payload (. token getPayload)]
         (log "Logging in : " (. payload getSubject))
-        payload))))
-  
+        {:subject (. payload getSubject)
+         :email (. payload getEmail)}))))
+
+(defn resolve-binding[val auth-str]
+  (condp = val
+    :idtoken
+    `(if (seq ~auth-str)
+       (let [[~'_ idtoken#] (clojure.string/split ~auth-str #"\s")]
+         (if (seq idtoken#) (validate-token idtoken#) {}))
+       {})
+    val))
+         
+(defn registered?[{:keys [email]}]
+  (#{"abstractalchemist@gmail.com"} email))
+                 
+
+(defn admin?[{:keys [email]}]
+  (#{"abstractalchemist@gmail.com"} email))
+             
+
+(defn m-login[[binding val & other] auth-str body]
+  (if (seq other)
+    `((fn[~binding]
+        ~(m-login other auth-str body))
+      ~(resolve-binding val auth-str))
+    `((fn[~binding]
+        ~@body)
+      ~(resolve-binding val auth-str))))
+
+(defmacro letlogin[bindings auth-str & body]
+  (m-login bindings auth-str body))
 
 (defroutes app-routes
+  (POST "/register" { {:strs [authorization]} :headers}
+        "This is the registration endpoint;  it requires a google JWT token and we'll contact you later")
+  (context "/anime" { {:strs [authorization] :as h} :headers body :body}
+           (GET "/images" []
+                (log "Grabbing images for gallery")
+                (letlogin [login :idtoken] authorization
+                          (if (and login (registered? login))
+                            {:status 200
+                             :headers {"Content-Type" "application/json"}
+                             :body (json/generate-string [{ :img "img1.jpg"}
+                                                          { :img "img2.jpg"}
+                                                          { :img "img3.jpg"}
+                                                          { :img "img4.jpg"}
+                                                          { :img "img5.jpg"}])}
+                            {:status 401})))
+                             
+           (GET "/" []
+         ;;       (let [[_ idtoken] (when (seq authorization) (clojure.string/split authorization #"\s"))
+                ;;             login (if (seq idtoken) (validate-token idtoken) {})]
+                (letlogin [login :idtoken
+                           admin (admin? login)] authorization
+                          (log "getting anime related info with auth <" login ">")
+                          
+                          {:status 200
+                           :headers {"Content-Type" "application/json"}
+                           :body (json/generate-string [{:title "Getting Started" :entry "" :img "getting-started.jpg" :editable  admin }
+                                                        {:title "The Perfect Insider"  :entry "" :img "the_perfect_insider.jpg"  :editable  admin}
+                                                        {:title "Tesagure! Bukatsumono" :entry "" :img "Tesagure-Bukatsumono.jpg"  :editable  admin}
+                                                        {:title "Gate" :entry "" :img "Gate-opens.png"  :editable  admin}])}))
+           (POST "/" {})
+           (PUT "/" {}))
+  (context "/programming" { {:keys [Authorization]} :headers}
+           (GET "/" []))
+  (context "/frc" { {:keys [Authorization]} :headers}
+           (GET "/" []))
   (GET "/" [] "Hello World")
-  (POST "/signin" {:keys [body]}
-        (let [{:keys [idtoken]} (json/parsed-seq body)]          
-          ))
-  (POST "/signout" [] {:status 500})
   (route/not-found "Not Found"))
 
-(defn check-signed-in
-  ""
-  [handler]
-  (fn[{{} :cookies :as req}]
-    (handler req)))
-
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-cors 
+   (wrap-defaults app-routes (assoc-in site-defaults [:security :anti-forgery] false))
+   :access-control-allow-origin [#"http://localhost:8000"]
+   :access-control-allow-methods [:get :put :post :delete]))
