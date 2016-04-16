1e9a4a61aa5770fcf5cecd9469bbfdbbe10c1708
Jason Hirata <jhirata@integrity-apps.com>
2016-04-16 13:06:10 -1000

    updated handler to retrieve images from a directory

--- src/data/handler.clj
+++ src/data/handler.clj
@@ -104,6 +104,14 @@
 (def animedb "http://localhost:5984/animedb")
 (def programmingdb "http://localhost:5984/programmingdb")
 
+(defn m-get-next-id
+  "gets the next available id in the couchdb"
+  [] 
+  (let [{:keys [body]} (client/get (str animedb "/_all_docs"))
+        {:strs [total_rows] :as query} (json/parse-string body)]
+    (clojure.pprint/pprint query)
+    total_rows))
+
 (defroutes app-routes
   (POST "/authorize" {{:strs [authorization]} :headers}
         (try
@@ -126,18 +134,18 @@
                                 (data.sample/populate-sample-data)
                                 {:status 200}))
            (GET "/images/:id" [id]
-                "Retriveing image id")
+                {:status 200
+                 :headers { "content-type" "application/octet-stream" }
+                 :body (clojure.java.io/input-stream (java.net.URLDecoder/decode id))})
            (GET "/images" []
                 (log "Grabbing images for gallery")
                 (letlogin [login :idtoken] authorization
                           (if (and login (registered? login))
                             {:status 200
                              :headers {"Content-Type" "application/json"}
-                             :body (json/generate-string [{ :id 0 :img "img1.jpg"}
-                                                          { :id 0 :img "img2.jpg"}
-                                                          { :id 0 :img "img3.jpg"}
-                                                          { :id 0 :img "img4.jpg"}
-                                                          { :id 0 :img "img5.jpg"}])}
+                             :body (json/generate-string (map (comp (fn[d] {:id (java.net.URLEncoder/encode d "UTF-8") :path d}) str) (java.nio.file.Files/newDirectoryStream
+                                                                                                                                       (java.nio.file.Paths/get "/" (into-array String ["home" "jmhirata" "Pictures"]))
+                                                                                                                                       "*.{png,jpg}")))}
                             {:status 401})))
            (GET "/:id" [id]
                 (letlogin [login :idtoken
@@ -173,8 +181,18 @@
                                       (json/generate-string
                                        (map retrieve rows))))}))
                                     
-           (POST "/" {})
-           (PUT "/" {}))
+           (PUT "/" {:keys [body]}
+                (letlogin [login :idtoken
+                           admin (admin? login)] authorization
+
+                           (let [next-id (m-get-next-id)
+                                 input (slurp (clojure.java.io/reader body))]
+                             (log "body: " input)
+                             (let [{:keys [status body]} (client/put (str animedb "/" next-id) {:headers {"content-type" "application/json"}
+                                                                                                :body input})]
+                               (log "result: " status " with body " body)
+                               {:status status})))))
+  
   (context "/programming" { {:keys [Authorization]} :headers}
            (GET "/" []))
   (context "/frc" { {:keys [Authorization]} :headers}
